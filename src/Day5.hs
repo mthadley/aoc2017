52568947807013a00d86829b17a95684299963dd
Michael Hadley <mikethadley@gmail.com>
2017-12-05 15:23:11 -0800

    Day 5

---
+++ src/Day5.hs
@@ -0,0 +1,51 @@
+module Day5 where
+
+import qualified Data.Vector.Mutable as M (modify)
+import Data.Maybe (fromMaybe, isNothing)
+import Text.Read (readMaybe)
+import Data.Vector (Vector, fromList, (!?), (//), imap, modify)
+
+solution :: IO (Int, Int)
+solution = do
+  jumps <- getInputJumps
+  return (jumpCountAdd jumps, jumpCountAddThree jumps)
+
+data CPU = CPU
+  { getJumps :: Vector Int
+  , getIndex :: Int
+  }
+
+initCPU :: Vector Int -> CPU
+initCPU jumps = CPU jumps 0
+
+jumpCountAdd :: Vector Int -> Int
+jumpCountAdd = jumpCountWith $ (+) 1
+
+jumpCountAddThree :: Vector Int -> Int
+jumpCountAddThree = jumpCountWith addThree
+
+jumpCountWith :: (Int -> Int) -> Vector Int -> Int
+jumpCountWith f = runToCompletion 0 . initCPU
+  where
+    runToCompletion :: Int -> CPU -> Int
+    runToCompletion count cpu = fromMaybe count $ do
+      newCPU <- nextState f cpu
+      return $ runToCompletion (count + 1) newCPU
+
+nextState :: (Int -> Int) -> CPU -> Maybe CPU
+nextState f (CPU jumps index) = do
+  jump <- jumps !? index
+  return $ CPU (updateAt f index jumps) $ index + jump
+
+addThree :: Int -> Int
+addThree jump = if jump >= 3
+                 then jump - 1
+                 else jump + 1
+
+updateAt :: (Int -> Int) -> Int -> Vector Int -> Vector Int
+updateAt f index = modify (\v -> M.modify v f index )
+
+getInputJumps :: IO (Vector Int)
+getInputJumps = do
+  jumps <- lines <$> readFile "src/day5_jumps.txt"
+  return $ fromList $ fromMaybe 0 <$> readMaybe <$> jumps
--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ import qualified Day1
 import qualified Day2
 import qualified Day3
 import qualified Day4
+import qualified Day5
 
 solutions :: [IO (Int, Int)]
 solutions =
@@ -15,6 +16,7 @@ solutions =
   , Day2.solution
   , Day3.solution
   , Day4.solution
+  , Day5.solution
   ]
 
 main :: IO ()
