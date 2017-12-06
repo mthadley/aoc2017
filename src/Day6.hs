4046671dd9d2dc2c34763ad7a5cdf0239024610f
Michael Hadley <mikethadley@gmail.com>
2017-12-06 14:38:28 -0800

    Still fast with a Seq

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,21 +1,20 @@
 module Day6 where
 
 import Data.Set (insert, member, singleton)
-import qualified Data.Vector.Mutable as M (modify, write)
 import Data.Maybe (fromMaybe)
-import Data.Vector (Vector, elemIndex, fromList, modify, (!))
+import Data.Sequence (Seq, adjust, elemIndexL, fromList, index, update)
 
 solution :: IO (Int, Int)
 solution = do
   return (distToFirstCycle input, findCycleLength input)
 
-input :: Vector Int
+input :: Seq Int
 input = fromList [5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6]
 
-distToFirstCycle :: Vector Int -> Int
+distToFirstCycle :: Seq Int -> Int
 distToFirstCycle = fst . findRepeat
 
-findCycleLength :: Vector Int -> Int
+findCycleLength :: Seq Int -> Int
 findCycleLength input = countDist 1 repeatedBank
   where
     repeatedBank = snd $ findRepeat input
@@ -24,23 +23,23 @@ findCycleLength input = countDist 1 repeatedBank
       | otherwise = countDist (count + 1) newBanks
         where newBanks = redistribute banks
 
-findRepeat :: Vector Int -> (Int, Vector Int)
-findRepeat = countDist $ singleton input
+findRepeat :: Seq Int -> (Int, Seq Int)
+findRepeat input = countDist (singleton input) input
   where
     countDist set banks
       | member newBanks set = (length set, newBanks)
       | otherwise = countDist (insert newBanks set) newBanks
         where newBanks = redistribute banks
 
-redistribute :: Vector Int -> Vector Int
-redistribute banks = applyToBanks (maxIndex + 1) (banks ! maxIndex) newBanks
+redistribute :: Seq Int -> Seq Int
+redistribute banks = applyToBanks (maxIndex + 1) (index banks maxIndex) newBanks
   where
-    maxIndex = fromMaybe 0 $ (flip elemIndex) banks $ maximum banks
-    newBanks = modify (\v -> M.write v maxIndex 0) banks
+    maxIndex = fromMaybe 0 $ (flip elemIndexL) banks $ maximum banks
+    newBanks = update maxIndex 0 banks
 
-applyToBanks :: Int -> Int -> Vector Int -> Vector Int
+applyToBanks :: Int -> Int -> Seq Int -> Seq Int
 applyToBanks _ 0 banks = banks
 applyToBanks startIndex value banks =
   applyToBanks (index + 1) (value - 1)
-    $ modify (\v -> M.modify v ((+) 1) index) banks
+    $ adjust ((+) 1) index banks
       where index = startIndex `mod` length banks
