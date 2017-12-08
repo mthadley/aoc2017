11b3e72eb0a8daf59e4f5c4cda3cf18cf8cd5350
Michael Hadley <mikethadley@gmail.com>
2017-12-08 01:25:58 -0800

    Day 7 part 2

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,18 +1,63 @@
 module Day7 where
 
-import Data.List (find)
-import Data.Maybe (fromMaybe)
+import qualified Data.Map as M
+import Data.List (find, groupBy, sortBy)
+import Data.Maybe (fromMaybe, mapMaybe)
 import qualified Data.Set as S
 import Text.ParserCombinators.ReadP as P
+import Control.Applicative ((<|>))
+import Control.Monad (msum)
+import Data.Function (on)
 
 solution :: IO (String, String)
 solution = do
-  input <- getTowers
-  return (findRoot input , "0")
+  input <- readTowers
+  return
+    ( findRootName input
+    , show $ findCorrectWeight input
+    )
 
-findRoot :: [Tower] -> String
-findRoot input = fromMaybe "" $ fmap getName $ find (not . (flip S.member) set . getName) input
+-- Part 1
+
+findRootName :: [Tower] -> String
+findRootName = fromMaybe "Not Found" . fmap getName . findRoot
+
+findRoot :: [Tower] -> Maybe Tower
+findRoot input = find (not . (flip S.member) set . getName) input
   where set = S.fromList $ concat $ map getLeaves input
+
+-- Part 2
+
+findCorrectWeight :: [Tower] -> Maybe Int
+findCorrectWeight towers = findUnbalancedTower =<< findRoot towers
+  where
+    towerMap = M.fromList $ map (\t -> (getName t, t)) towers
+    findUnbalancedTower tower =
+      (msum $ map findUnbalancedTower leaves)
+      <|>
+      correctWeight towerMap leaves <$ (find (((/=) targetWeight) . towerWeight towerMap) leaves)
+        where
+          targetWeight = getTargetWeight towerMap tower
+          leaves = getTowers towerMap $ getLeaves tower
+
+correctWeight :: M.Map String Tower -> [Tower] -> Int
+correctWeight towerMap towers = towerWeight towerMap good - badLeavesWeight
+  where
+    [[bad], good:_] = sortBy (compare `on` length)
+      $ groupBy (\a b -> towerWeight towerMap a == towerWeight towerMap b) towers
+    badLeavesWeight = sum $ map (towerWeight towerMap) $ getTowers towerMap $ getLeaves bad
+
+getTargetWeight :: M.Map String Tower -> Tower -> Int
+getTargetWeight towerMap tower@(Tower _ weight leaves) =
+  (towerWeight towerMap tower - weight) `div` (length leaves)
+
+towerWeight :: M.Map String Tower -> Tower -> Int
+towerWeight towerMap (Tower _ weight leaves) = weight + leavesWeight
+  where
+    leavesWeight = sum $ map (towerWeight towerMap) $ getTowers towerMap leaves
+
+getTowers :: M.Map String Tower -> [String] -> [Tower]
+getTowers towerMap = mapMaybe ((flip M.lookup) towerMap)
 
 data Tower = Tower
   { getName :: String
@@ -22,8 +67,8 @@ data Tower = Tower
 
 -- File Parser
 
-getTowers :: IO [Tower]
-getTowers = do
+readTowers :: IO [Tower]
+readTowers = do
   input <- readFile "src/day7_towers.txt"
   return $ fst $ head $ P.readP_to_S towers input
 
