46665e1856042352f7ec57060256001db1872ff1
Michael Hadley <mikethadley@gmail.com>
2017-12-08 17:44:19 -0800

    WIP

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,20 +1,21 @@
 module Day7 where
 
-import qualified Data.Map as M
-import Data.List (find, groupBy, sortBy)
-import Data.Maybe (fromMaybe, mapMaybe)
-import qualified Data.Set as S
-import Text.ParserCombinators.ReadP as P
 import Control.Applicative ((<|>))
 import Control.Monad (msum)
+import Control.Monad.Reader (Reader, runReader, ask)
 import Data.Function (on)
+import Data.List (find, groupBy, sortBy)
+import Data.Maybe (fromMaybe, mapMaybe, isJust)
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Text.ParserCombinators.ReadP as P
 
 solution :: IO (String, String)
 solution = do
   input <- readTowers
   return
     ( findRootName input
-    , show $ findCorrectWeight input
+    , "23"
     )
 
 -- Part 1
@@ -28,36 +29,39 @@ findRoot input = find (not . (flip S.member) set . getName) input
 
 -- Part 2
 
-findCorrectWeight :: [Tower] -> Maybe Int
-findCorrectWeight towers = findUnbalancedTower =<< findRoot towers
+findCorrectWeight towers = ((runReader findUnbalancedTower) towerMap) =<< findRoot towers
   where
     towerMap = M.fromList $ map (\t -> (getName t, t)) towers
-    findUnbalancedTower tower =
-      (msum $ map findUnbalancedTower leaves)
-      <|>
-      correctWeight towerMap leaves <$ (find (((/=) targetWeight) . towerWeight towerMap) leaves)
-        where
-          targetWeight = getTargetWeight towerMap tower
-          leaves = getTowers towerMap $ getLeaves tower
+    findUnbalancedTower tower = do
+      targetWeight <- getTargetWeight tower
+      leaves <- getTowers $ getLeaves tower
+      towerWeights <- mapM towerWeight leaves
+      (return $ msum $ map findUnbalancedTower leaves) <|> ((return $ correctWeight leaves) <$ (find (((/=) targetWeight)) towerWeights))
 
-correctWeight :: M.Map String Tower -> [Tower] -> Int
-correctWeight towerMap towers = towerWeight towerMap good - badLeavesWeight
-  where
-    [[bad], good:_] = sortBy (compare `on` length)
-      $ groupBy (\a b -> towerWeight towerMap a == towerWeight towerMap b) towers
-    badLeavesWeight = sum $ map (towerWeight towerMap) $ getTowers towerMap $ getLeaves bad
+correctWeight :: [Tower] -> TowerP Int
+correctWeight towers = do
+  towerWeights <- mapM towerWeight towers
+  let [[(_, bad)], (_, good):_] = sortBy (compare `on` length) $ groupBy (\(a, _) (b, _) -> a == b) $ zip towerWeights towers
+  goodWeight <- towerWeight  good
+  badLeavesWeights <- mapM towerWeight =<< (getTowers $ getLeaves bad)
+  return $ goodWeight - (sum badLeavesWeights)
 
-getTargetWeight :: M.Map String Tower -> Tower -> Int
-getTargetWeight towerMap tower@(Tower _ weight leaves) =
-  (towerWeight towerMap tower - weight) `div` (length leaves)
+getTargetWeight :: Tower -> TowerP Int
+getTargetWeight tower@(Tower _ weight leaves) = do
+  totalWeight <- towerWeight tower
+  return $ (totalWeight - weight) `div` (length leaves)
 
-towerWeight :: M.Map String Tower -> Tower -> Int
-towerWeight towerMap (Tower _ weight leaves) = weight + leavesWeight
-  where
-    leavesWeight = sum $ map (towerWeight towerMap) $ getTowers towerMap leaves
+towerWeight :: Tower -> TowerP Int
+towerWeight (Tower _ weight leaves) = do
+  leavesWeights <- mapM towerWeight =<< getTowers leaves
+  return $ weight + (sum leavesWeights)
 
-getTowers :: M.Map String Tower -> [String] -> [Tower]
-getTowers towerMap = mapMaybe ((flip M.lookup) towerMap)
+getTowers :: [String] -> TowerP [Tower]
+getTowers towers = do
+  towerMap <- ask
+  return $ mapMaybe ((flip M.lookup) towerMap) towers
+
+type TowerP a = Reader (M.Map String Tower) a
 
 data Tower = Tower
   { getName :: String
